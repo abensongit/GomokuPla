84fd3fadf1e3ccba2ebc3f3ed93cad36613323c7
david <david@davids-imac.local>
2018-11-05 12:57:19 +0800

    Initial Commit

---
+++ GomokuPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GomokuPlay
+//
+//  Created by david on 2018/11/5.
+//  Copyright © 2018年 HANSEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GomokuPlay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GomokuPlay
+//
+//  Created by david on 2018/11/5.
+//  Copyright © 2018年 HANSEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
